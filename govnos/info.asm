8929a16f22b2a8a291f90250f9a6a14e62479972
xi816 <jej07267@gmail.com>
2025-04-17 23:22:57 +0300

    Updated even more system info

--- govnos/info.asm
+++ govnos/info.asm
@@ -5,6 +5,6 @@ info_main:
 
 govnos_info: bytes "^[[96m"
              bytes "GovnOS version: 0.3.0-24$"
-             bytes "Release date: 2025-04-16$"
+             bytes "Release date: 2025-04-17$"
              bytes "(c) Xi816, 253*8+1$"
              bytes "^[[0m^@"
